e217043802636bb7381ef29ee9aaf2383c11aebf
Andrés <retardix456@gmail.com>
2022-01-25 08:34:00 -0500

    wow

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Landing from './components/Landing';
 
 const App = () => {
   return (
     <div>
-      <h1>Hey!</h1>
+      <Landing />
     </div>
   );
 };
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 
+import profile from '../assets/profile-pic.jpg'
+import './landing.css'
+
 const Landing = () => {
     return (
-        <div className='wrapper'>
-            
+        <div className='landing'>
+            <div className="wrapper">
+                <div className="text">
+                    <img src={profile} alt='Profile Picture' width='300px'/>
+                    <br />
+                    <h1>Andrés Rodríguez</h1>
+                    <blockquote>-Full-Stack Web Developer-</blockquote>
+                    <br /><br />
+                    <p>I'm a Software Engineering student from Colombia. I started learning Front-end development
+                        with React. Later, I decided to become a Full-Stack developer mainly with React using the MERN Stack.
+                    </p>
+                </div>
+            </div>
         </div>
     );
 };
