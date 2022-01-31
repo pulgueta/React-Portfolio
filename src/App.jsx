5fdc29fd9ed73d00e30bd853c4b56f3e307855dd
Andrés <retardix456@gmail.com>
2022-01-31 12:58:46 -0500

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import Landing from './components/Landing';
-import NotFound from './components/NotFound';
-import ImageSlider from './components/ImageSlider';
-import { SliderData } from './components/SliderData';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
+import Landing from "./components/Landing";
+import NotFound from "./components/NotFound";
+import ImageSlider from "./components/ImageSlider";
+import { SliderData } from "./components/SliderData";
 
 const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/' element={<Landing text='Andrés Rodríguez' />} />
-        <Route path='/pictures' element={<ImageSlider slides={SliderData} />} />
+        <Route path="/" element={<Landing text="Andrés Rodríguez" />} />
+        <Route path="/pictures" element={<ImageSlider slides={SliderData} />} />
 
-        <Route path='*' element={<NotFound />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -1,52 +1,65 @@
-import React, { useState, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { AnimatePresence, motion } from "framer-motion";
 
-import profile from '../assets/profile-pic.jpg'
-import './landing.css'
+import profile from "../assets/profile-pic.jpg";
+import "./landing.css";
 
 const Landing = ({ text }) => {
-    const [fadeProp, setFadeProp] = useState({
-        fade: 'fade-out',
-    })
+  const [fadeProp, setFadeProp] = useState({
+    fade: "fade-out",
+  });
 
-    useEffect(() => {
-        const timeout = setInterval(() => {
-            if (fadeProp.fade === 'fade-out') {
-                setFadeProp({
-                    fade: 'fade-in'
-                })
-            }
-        }, 600)
+  useEffect(() => {
+    const timeout = setInterval(() => {
+      if (fadeProp.fade === "fade-out") {
+        setFadeProp({
+          fade: "fade-in",
+        });
+      }
+    }, 600);
 
-        return () => clearInterval(timeout)
-    }, [fadeProp])
+    return () => clearInterval(timeout);
+  }, [fadeProp]);
 
-    const navigate = useNavigate()
+  const navigate = useNavigate();
 
-    const redirect = () => {
-        navigate('/pictures')
-    }
+  const redirect = () => {
+    navigate("/pictures");
+  };
 
-    return (
-        <div className='landing'>
-            <div className="wrapper">
-                <div className="text">
-                    <img className={fadeProp.fade} src={profile} alt='Profile Picture' />
-                    <br />
-                    <h1 className={fadeProp.fade}>{text}</h1>
-                    <blockquote className={fadeProp.fade}>-Front-End Developer and Photographer-</blockquote>
-                    <br /><br />
-                    <p className={fadeProp.fade}>I am a Software Engineering student from Colombia. My main focus is the Front-End Development, but I also
-                        learn about Cybersecurity, I study from both topics to become a hacker and a better web developer. I also practice
-                        photography and videography. Click the button to check some of my photos.
-                    </p>
-                    <br />
-                    <button id='text-btn' className={fadeProp.fade} onClick={redirect}>Click me!</button>
-                </div>
-            </div>
+  return (
+    <motion.div
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+      className="landing"
+    >
+      <div className="wrapper">
+        <div className="text">
+          <img className={fadeProp.fade} src={profile} alt="Profile Picture" />
+          <br />
+          <h1 className={fadeProp.fade}>{text}</h1>
+          <blockquote className={fadeProp.fade}>
+            -Front-End Developer and Photographer-
+          </blockquote>
+          <br />
+          <br />
+          <p className={fadeProp.fade}>
+            I am a Software Engineering student from Colombia. My main focus is
+            the Front-End Development, but I also learn about Cybersecurity, I
+            study from both topics to become a hacker and a better web
+            developer. I also practice photography and videography. Click the
+            button to check some of my photos.
+          </p>
+          <br />
+          <button id="text-btn" className={fadeProp.fade} onClick={redirect}>
+            Click me!
+          </button>
         </div>
-    );
-
+      </div>
+    </motion.div>
+  );
 };
 
 export default Landing;
